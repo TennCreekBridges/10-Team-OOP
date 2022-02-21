b7fd6d2a5d66c967217952031419a042c9a86506
TennCreekBridges <bridgettvalet@gmail.com>
2022-02-21 16:05:08 -0600

    adds classes to library

---
+++ index.js
@@ -0,0 +1,12 @@
+// required modules
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// constructors
+
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+// generate markdown
