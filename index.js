ba8e2c3ba32e5a6baa97ed43b637ed7d62146ab5
TennCreekBridges <bridgettvalet@gmail.com>
2022-02-21 20:59:55 -0600

    adds switch case to index.js and adds placeholder for writing content to page

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 // required modules
 const fs = require('fs');
-const inquirer = require('inquirer');
+const prompts = require('./utils/prompts');
+const generatePage = require('.utils/generatePage');
 
 // constructors
-
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const { rejects } = require('assert');
 
-// generate markdown
+// adds team info to array and evaluates statement to execute based on case 
+prompts().then((teamInfo) => {
+    let people = [];
+    teamInfo.forEach((element) => {
+        switch (element.role) {
+            case 'Manager':
+                const manager = new Manager(element);
+                people.push(manager);
+                return;
+            case 'Engineer':
+                const engineer = new Engineer(element);
+                people.push(engineer);
+                return;
+            case 'Intern':
+                const intern = new Intern(element);
+                people.push(intern);
+                return;
+        }
+    });
+
+// writes file to ./dist/index.html 
+    fs.writeFile(`./dist/index.html`, generatePage(people), (err) => {
+        if (err) {
+            rejects(err);
+            return;
+        }
+    });
+});
