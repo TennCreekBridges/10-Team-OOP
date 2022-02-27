2d650da988b16088c759b31f0bb0a8043fe6f66b
TennCreekBridges <bridgettvalet@gmail.com>
2022-02-27 14:14:57 -0600

    updates error in questions.js

--- utils/questions.js
+++ utils/questions.js
@@ -14,84 +14,79 @@ Add A Team Member
 `);
 
 return inquirer
-    .prompt[(
-        {
-            type: "input",
-            name: "name",
-            message: "What is the team member name?",
-        },
-        {   type: "input",
-            name: "id",
-            message: "What is the team member ID?",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your team member's email address?",
-        },
-        {
-           type: "list",
-           name: "occupation",
-           message: "What is the team member occupation? Select one.",
-           choices: ["Manager", "Engineer", "Intern"],
-        },
-        {
-           type: "input",
-           name: "office",
-           message: "What is the manager's office number?",
-           when: ({ occupation }) => {
-               if (occupation === "Manager") {
-                   return true;
-               } else {
-                   return false;
-               }
-           },
-        },
-        {
-           type: "input",
-           name: "github",
-           message: "What is the engineer\'s GitHub account name?",
-           when: ({ occupation }) => {
-               if (occupation === "Intern") {
-                   return true;
-               } else {
-                   return false;
-               }
-            },
-        },
-        {
-           type: "input",
-           name: "school",
-           message: "What is the intern's school?",
-           when: ({ occupation }) => {
-               if (occupation === "Intern") {
-                   return true;
-               } else {
-                   return false;
-               }
-           },
-        },
-        {
-           type: "confirm",
-           name: "confirmAnother",
-           message: "Any additional team members?",
-           default: false,
-        },
-    ])    
-
-    .then((person) => {
-        teamData.push(person);
-        if (person.confirmAnother) {
-            return promptPeople(teamData);
-        } else {
-            return teamData;
-        }
-    });
+.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "What is the team member name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the team member ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the team member email?",
+  },
+  {
+    type: "list",
+    name: "occupation",
+    message: "What is the team member occupation? Select one.",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    name: "office",
+    message: "What is the manager's office number?",
+    when: ({ occupation }) => {
+      if (occupation === "Manager") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineer's GitHub account name?",
+    when: ({ occupation }) => {
+      if (occupation === "Engineer") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What is your intern's school?",
+    when: ({ occupation }) => {
+      if (occupation === "Intern") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  {
+    type: "confirm",
+    name: "confirmAnother",
+    message: "Any additional team members?",
+    default: false,
+  },
+])
+.then((person) => {
+  teamData.push(person);
+  if (person.confirmAnother) {
+    return promptPeople(teamData);
+  } else {
+    return teamData;
+  }
+});
 };
 
 module.exports = promptPeople;
-
-
-
-
-
