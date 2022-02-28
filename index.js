ab5193eb2a2cd8b3e9c5e9828f7b3f0c00f3c153
TennCreekBridges <bridgettvalet@gmail.com>
2022-02-27 21:08:11 -0600

    complete pivot

--- index.js
+++ index.js
@@ -1,40 +1,225 @@
-// required modules
+// required globals
 const fs = require('fs');
-const prompts = require('./utils/questions');
+const inquirer = require('inquirer');
 const generatePage = require('./utils/generatePage');
-
-// constructors
-const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
-const { rejects } = require('assert');
 
-// adds team info to array and evaluates statement to execute based on case 
-prompts().then((teamInfo) => {
-    let people = [];
-    teamInfo.forEach((element) => {
-        switch (element.role) {
-            case 'Manager':
-                const manager = new Manager(element);
-                people.push(manager);
-                return;
-            case 'Engineer':
-                const engineer = new Engineer(element);
-                people.push(engineer);
-                return;
-            case 'Intern':
-                const intern = new Intern(element);
-                people.push(intern);
-                return;
-        }
-    });
+// creates master employees array
+const employees = [];
 
-// writes file to ./dist/index.html 
-    fs.writeFile(`./dist/index.html`, generatePage(people), (err) => {
-        if (err) {
-            rejects(err);
-            return;
-        }
-    });
-});
+// question array to generate manager info
+const mgrQuestions = (mgrData) => {
+    return inquirer
+        .prompt([
+                {
+                    type: "input",
+                    name: "mgrName",
+                    message: "What is your manager's name? (Required)",
+                    validate: (mgrName) => {
+                        if (mgrName) {
+                            return true;
+                        } else {
+                            console.log("You must enter your manager's name. Try again.");
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "mgrId",
+                    message: "What is their employee ID? (Required)",
+                    validate: (mgrId) => {
+                        if (mgrId) {
+                            return true;
+                        } else {
+                            console.log("You must enter their ID. Try again.");
+                            return false;
+                        }
+                    }    
+                },
+                {
+                    type: "input",
+                    name: "mgrEmail",
+                    message: "What your manager's email address? (Required)",
+                    validate: (mgrEmail) => {
+                        if (mgrEmail) {
+                            return true;
+                        } else {
+                            console.log("You must enter your manager's email address. Try again.");
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "mgrOffice",
+                    message: "What is your manager's office number? (Required)",
+                    validate: (officeNum) => {
+                        if (officeNum) {
+                            return true;
+                        } else {
+                            console.log("You must enter your manager's office number. Try again.");
+                            return false;
+                        }
+                    }
+                },
+            ])
+            .then((data) => {
+                const manager = new Manager(
+                    data.mgrName,
+                    data.mgrId,
+                    data.mgrEmail,
+                    data.mgrOffice
+                );
+                employees.push(manager);
+            });
+};
+
+// question array to generate general employee class info
+const employeeQuestions = () => {
+    return inquirer
+        .prompt([
+                {
+                    type: "list",
+                    name: "empType",
+                    message: "Okay, got it. Now what type of underling would you like to add?",
+                    choices: ["Engineer", "Intern", "None"],
+                },
+                {
+                    type: "input",
+                    name: "empName",
+                    message: "What is your employee's name? (Required)",
+                    validate: (empName) => {
+                        if (empName) {
+                            return true;
+                        } else {
+                            console.log("You must enter your employee's name. Try again.");
+                            return false;
+                        }
+                    },
+                    when: ({ empType }) => {
+                        if (empType !== "None") {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }   
+                },
+                {
+                    type: "input",
+                    name: "empId",
+                    message: "What is your employee's ID? (Required)",
+                    validate: (empId) => {
+                        if (empId) {
+                            return true;
+                        } else {
+                            console.log("You must enter your employee's ID. Try again.");
+                            return false;
+                        }
+                    },
+                    when: ({ empType }) => {
+                        if (empType !== "None") {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "empEmail",
+                    message: "What is your employee's email address? (Required)",
+                    validate: (empEmail) => {
+                        if (empEmail) {
+                            return true;
+                        } else {
+                            console.log("You must enter employee's email address. Try again.");
+                            return false;
+                        }
+                    },
+                    when: ({ empType }) => {
+                        if (empType !== "None") {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } 
+                },
+                {
+                    type: "input",
+                    name: "egrGit",
+                    message: "What is your engineer's GitHub username? (Required)",
+                    validate: (egrGit) => {
+                        if (egrGit) {
+                            return true;
+                        } else {
+                            console.log("You must enter engineer's GitHub username. Try again.");
+                            return false;
+                        }
+                    },
+                    when: ({ empType }) => {
+                        if (empType === "Engineer") {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } 
+                },
+                {
+                    type: "input",
+                    name: "intSchool",
+                    message: "What is your intern's school? (Required)",
+                    validate: (intSchool) => {
+                        if (intSchool) {
+                            return true;
+                        } else {
+                            console.log("You must enter intern's school. Try again.");
+                            return false;
+                        }
+                    },
+                    when: ({ empType }) => {
+                        if (empType === "Intern") {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } 
+                }
+            
+        ])
+
+        // push engineer info to employee array and ask if user wants to create another employee
+        .then ((employeeInfo) => {
+            if (employeeInfo.empType === "Engineer") {
+                const engineer = new Engineer(
+                    employeeInfo.empName,
+                    employeeInfo.empId,
+                    employeeInfo.empEmail,
+                    employeeInfo.egrGit
+                );
+                employees.push(engineer);
+                return employeeQuestions();
+
+            // push intern info to employee array and ask if user wants to create another employee
+            } else if (employeeInfo.empType === "Intern") {
+                const intern = new Intern(
+                    employeeInfo.empName,
+                    employeeInfo.empId,
+                    employeeInfo.empEmail,
+                    employeeInfo.intSchool
+                );
+                employees.push(intern);
+                return employeeQuestions();
+
+            // write file to index.html in dist folder
+            } else {
+                fs.writeFileSync("./dist/index.html", generatePage(employees));
+                console.log("HTML file created! Check it out in the dist directory!")
+            }
+        });
+};        
+
+mgrQuestions()
+    .then(employeeQuestions)
--- utils/generatePage.js
+++ utils/generatePage.js
@@ -1,23 +1,126 @@
-function generatePage(people) {
-    return `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"/>
-      <title>Team OOP</title>
-  </head>
-  <body>
-    <header class="bg-info p-3 mb-4>
-    <h1 class="text-center text-white">Team OOP</h1>
-    </header>
-        <main>
+const createTeam = (team) => {
+	console.log("Team: ", team);
+
+    // create manager card
+	const managerCard = (manager) => {
+		return `
+        <div id="manager-main" class="col-12 col-md-6 col-xl-4 mb-4">
+        <div class="card">
+            <h3 class="card-header bg-warning bg-opacity-30 text-light text-center d-flex align-items-center">Manager<i class="fa fa-bullhorn p-2"></i></h3>
+            <p id="manager-content" class="text-center text-dark p-4 card-content">
+            ${manager.getName()}
+            <br>
+            Employee # ${manager.getId()}
+            <br>
+            <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a>
+            <br>
+            Office # ${manager.getOffice()}
+            </p>
+        </div>
+        </div>
+	`;
+	};
+
+    // create engineer card
+	const engineerCard = (engineer) => {
+		return `
+        <div id="engineer-main" class="col-12 col-md-6 col-xl-4 mb-4">
+        <div class="card">
+            <h3 class="card-header bg-warning bg-opacity-30 text-light text-center d-flex align-items-center">Engineer<i class="fa fa-clipboard p-2"></i></h3>
+            <p id="engineer-content" class="text-center text-dark p-4 card-content">
+            ${engineer.getName()}
+            <br>
+            Employee # ${engineer.getId()}
+            <br>
+            <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
+            <br>
+            GitHub: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a>
+            </p>
+        </div>
+        </div>
+	`;
+	};
+
+    //create intern card
+	const internCard = (intern) => {
+		return `
+        <div id="intern-main" class="col-12 col-md-6 col-xl-4 mb-4">
+        <div class="card">
+            <h3 class="card-header bg-warning bg-opaciy-30 text-light text-center d-flex align-items-center">Intern<i class="fa fa-mortar-board p-2"></i></h3>
+            <p id="intern-content" class="text-center text-dark p-4 card-content">
+            ${intern.getName()}
+            <br>
+            Employee # ${intern.getId()}
+            <br>
+            <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a>
+            <br>
+            ${intern.getSchool()}
+            </p>
+        </div>
+        </div>
+	`;
+	};
+
+	const html = [];
+
+	// Manager
+	html.push(
+		team
+			.filter((employee) => employee.getRole() === "Manager")
+			.map((manager) => managerCard(manager))
+	);
+
+	// Engineer
+	html.push(
+		team
+			.filter((employee) => employee.getRole() === "Engineer")
+			.map((engineer) => engineerCard(engineer))
+			.join("")
+	);
+
+	// Intern
+	html.push(
+		team
+			.filter((employee) => employee.getRole() === "Intern")
+			.map((intern) => internCard(intern))
+			.join("")
+	);
+	return html.join("");
+};
+
+const generatePage = (team) => {
+	return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+  
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <title>Team OOP</title>
+    </head>
+    <body>   
+        <header class="bg-success p-4 mb-5">
+                <h1 class="bg-success text-light text-center py-2"><i class="fa fa-id-card"></i> Team OOP <i class="fa fa-id-card"></i></h1>
+        </header>
+        <main class="col-9 d-flex flex-column container-fluid">
+        <div class="row justify-content-center">
+    
+        ${createTeam(team)}
         </main>
-  </body>
-</html>`;
-}
+        <hr/>
+        <footer class="bg-transparent mt-auto d-small-flex">
+        <h6 class="text-center"><i class="fa fa-share p-2"></i>Cobbled together by <a href="https://https://github.com/TennCreekBridges/10-Team-OOP">Bridgett Valet</a>
+        <br>
+        &copy 2022 Mad Company, Inc.<i class="fa fa-cogs p-2"></i>
+        </h6>
+        </footer>
+    </body>
+    </html>
+    `;
+};
 
 module.exports = generatePage;
